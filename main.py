d3050dd5de6a4478d52e56ffe79db2fb055888ab
Sebastian Henao <sebastianhenaoloaiza@gmail.com>
2021-12-27 14:49:14 -0500

    Nueva funcion creada y nuevo archivo

--- main.py
+++ main.py
@@ -1,37 +1,50 @@
-from openpyxl import Workbook, load_workbook
-from openpyxl.worksheet import worksheet
+import os
 import json
+from openpyxl import load_workbook
+from openpyxl.worksheet import worksheet
 
-# Configuraciones Iniciales
+# Configuraciones Iniciales de openpyxl
 workbook = load_workbook('./document/datos.xlsx')
 worksheet = workbook.active
 
-i = 0
 
-for row in worksheet.values:
+def read_excel(i):
+    i = i
 
-    # Omito la primera fila que contiene los encabezados en el archivo excel.
-    if row[0] == 'Tipo Doc':
-        continue
+    # TODO: Averiguar como iniciar recorrido desde el numero que esta en counter
+    for row in worksheet.values:
 
-    # Transformo los datos en un diccionario
-    data = {
-        "tipo_doc" : row[0],
-        "documento" : row[1],
-        "fecha_atencion" : row[2],
-        "tipo_atencion" : row[3],
-        "medico" : row[4],
-        "dx" : row[5],
-        "descripcion_dx" : row[6],
-        "lateralidad" : row[7],
-        "av" : row[6],
-        "tipo_av" : row[8],
-        "emc" : row[9],
-        "av_lb" : row[10],
-        "observaciones" : row[11],
-        "eps" : row[12],
-    }
+        # Omito la primera fila que contiene los encabezados en el archivo excel.
+        if row[0] == 'Tipo Doc':
+            continue
 
-    i+= 1
+        # Transformo los datos en un diccionario
+        data = {
+            "tipo_doc" : row[0],
+            "documento" : row[1],
+            "fecha_atencion" : row[2],
+            "tipo_atencion" : row[3],
+            "medico" : row[4],
+            "dx" : row[5],
+            "descripcion_dx" : row[6],
+            "lateralidad" : row[7],
+            "av" : row[6],
+            "tipo_av" : row[8],
+            "emc" : row[9],
+            "av_lb" : row[10],
+            "observaciones" : row[11],
+            "eps" : row[12],
+        }
 
-    print(i)
+        i+= 1
+
+    i = str(i)
+
+    # Configuraciones iniciales de OS
+    counter = open('./counter.txt', 'w')
+    counter.write(i)
+    counter.close
+
+    return i
+
+print(read_excel(5))
