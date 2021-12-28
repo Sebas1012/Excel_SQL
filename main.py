201efece27d9be8a0ea96a69dd1ca126a510968c
Sebastian Henao <sebastianhenaoloaiza@gmail.com>
2021-12-27 20:23:32 -0500

    ♻️ Refactorizacion

--- main.py
+++ main.py
@@ -1,50 +1,46 @@
-import os
 import json
 from openpyxl import load_workbook
-from openpyxl.worksheet import worksheet
 
 # Configuraciones Iniciales de openpyxl
 workbook = load_workbook('./document/datos.xlsx')
-worksheet = workbook.active
+sheet = workbook.active
 
+# Lectura del archivo counter, el cual almacena la ultima fila que se itero para poder continuar desde la misma.
+counter = open('./counter.txt', 'r')
+i = int(counter.read())
+j = 0
 
-def read_excel(i):
-    i = i
+for row in sheet.iter_rows(min_row=i, values_only=True):
 
-    # TODO: Averiguar como iniciar recorrido desde el numero que esta en counter
-    for row in worksheet.values:
-
-        # Omito la primera fila que contiene los encabezados en el archivo excel.
-        if row[0] == 'Tipo Doc':
-            continue
-
-        # Transformo los datos en un diccionario
-        data = {
+    # Dicionario para ordenar los datos obtenidos de la lectura del archivo excel.
+    data = {
             "tipo_doc" : row[0],
             "documento" : row[1],
             "fecha_atencion" : row[2],
-            "tipo_atencion" : row[3],
-            "medico" : row[4],
-            "dx" : row[5],
-            "descripcion_dx" : row[6],
-            "lateralidad" : row[7],
-            "av" : row[6],
-            "tipo_av" : row[8],
-            "emc" : row[9],
-            "av_lb" : row[10],
-            "observaciones" : row[11],
-            "eps" : row[12],
+            "tipo_atencion" : row[5],
+            "medico" : row[6],
+            "dx" : row[7],
+            "descripcion_dx" : row[8],
+            "lateralidad" : row[9],
+            "av" : row[10],
+            "tipo_av" : row[11],
+            "emc" : row[12],
+            "av_lb" : row[13],
+            "observaciones" : row[14],
+            "eps" : row[15],
         }
+    
+    j+=1
+    print(data)
 
-        i+= 1
 
-    i = str(i)
+j = j + i
+j = str(j)
 
-    # Configuraciones iniciales de OS
-    counter = open('./counter.txt', 'w')
-    counter.write(i)
-    counter.close
+# Escritura en el archivo counter de la ultima fila iterada para poder contunuar desde la misma si hay actualizaciones en el archivo.
+counter = open('./counter.txt', 'w')
+counter.write(j)
+counter.close()
 
-    return i
-
-print(read_excel(5))
+    
+    
